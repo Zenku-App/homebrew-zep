2676463d12d829a5238b5ed6a4dbbfcf6f7ee5b0
Kai Zhao <kaiz11@outlook.com>
2026-05-31 04:11:30 +0000

    cask zep-desktop: v2026.5.1

--- Casks/zep-desktop.rb
+++ Casks/zep-desktop.rb
@@ -1,8 +1,8 @@
 cask "zep-desktop" do
   version "2026.5.1"
-  sha256 "4dfca1bd0930c01755a087e300d1e6443644047ef78c3692701cf717326c5c76"
+  sha256 "4bda8cde8b02a7c007bc82d7cbf6a58d17d82e3b7cb8e51ea68477f04b654098"
 
-  url "https://github.com/Zenku-App/homebrew-zep/releases/download/v2026.5.1/Zep_0.1.0_aarch64.dmg"
+  url "https://github.com/Zenku-App/homebrew-zep/releases/download/v2026.5.1/Zep_2026.5.1_aarch64.dmg"
   name "Zep"
   desc "Zep desktop app"
   homepage "https://github.com/Zenku-App/homebrew-zep"
